9dae368168432acba21d3f05d541ac5fc8bfbab0
Abhijeet-Chandra <ajiteshchandra927@gmail.com>
2026-06-17 18:33:03 +0530

    Update LC70.java

--- Extras/DP/LC70.java
+++ Extras/DP/LC70.java
@@ -9,3 +9,19 @@ class Solution {
         return helper(0,n);
     }
 }
+
+//rec + memo
+class Solution {
+    int [] dp;
+    public int helper(int steps, int n){
+        if(steps > n)return 0;
+        if(steps == n) return 1;
+        if(dp[steps] != -1)return dp[steps];
+        return dp[steps] = helper(steps+1,n)+helper(steps+2,n);
+    }
+    public int climbStairs(int n) {
+        dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return helper(0,n);
+    }
+}
