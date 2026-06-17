e4a77a42dbdcbf848beccb4e788cd12771da28e3
Abhijeet-Chandra <ajiteshchandra927@gmail.com>
2026-06-17 16:44:01 +0530

    Update LC509.java

--- Extras/DP/LC509.java
+++ Extras/DP/LC509.java
@@ -1,3 +1,4 @@
+//using rec + memo
 class Solution {
     int [] dp;
     public int helper(int n){
@@ -11,6 +12,19 @@ class Solution {
         if(n>=0)dp[0]=0;
         if(n>=1)dp[1]=1;
         helper(n);
+        return dp[n];
+    }
+}
+
+//using bottom up:
+class Solution {
+    public int fib(int n) {
+        int [] dp = new int[n+1];
+        dp[0] = 0;
+        if(n>=1)dp[1] = 1;
+        for(int i = 2; i<=n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
         return dp[n];
     }
 }
